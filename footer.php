1842f3e9bae32ea6593abb9fb3dc2fa26b2a094c
Tony Edwards <tony.edwards@gmail.com>
2021-01-30 13:59:29 +0000

    Adds the footer partial

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+/* ------------------------------------------------------------------------ */
+/* Theme Footer
+/* ------------------------------------------------------------------------ */
+?>
+<footer>
+    <div class="container flex">
+        <p>© <a href="https://thatseagullbob.co.uk" title="That Seagull Bob Homepage">That Seagull Bob <span id="current-year">2020</span></a></p>
+        <p>Company no: 11844082<br /><a href="https://ecologi.com/thatseagullbob" title="Ecologi Tree Planting">Visit our forest</a></p>
+    </div>
+</footer>
+
+<?php wp_footer() ?>
--- index.php
+++ index.php
@@ -66,13 +66,7 @@
 
 		</div>
 
-		<footer>
-			<div class="container flex">
-				<p>© <a href="https://thatseagullbob.co.uk" title="That Seagull Bob Homepage">That Seagull Bob <span id="current-year">2020</span></a></p>
-				<p>Company no: 11844082<br /><a href="https://ecologi.com/thatseagullbob" title="Ecologi Tree Planting">Visit our forest</a></p>
-			</div>
-		</footer>
-
-		<?php wp_footer() ?>
+		<?php get_footer(); ?>
+		
 	</body>
 </html>
