a0d8971807812f8f96ca86ac8eb89211e80e8e99
Tony Edwards <tony.edwards@gmail.com>
2021-01-30 14:34:58 +0000

    Add bootstrap layout to footer

--- footer.php
+++ footer.php
@@ -3,10 +3,15 @@
 /* Theme Footer
 /* ------------------------------------------------------------------------ */
 ?>
-<footer>
-        <p>© <a href="https://thatseagullbob.co.uk" title="That Seagull Bob Homepage">That Seagull Bob <span id="current-year">2020</span></a></p>
-        <p>Company no: 11844082<br /><a href="https://ecologi.com/thatseagullbob" title="Ecologi Tree Planting">Visit our forest</a></p>
+<footer class="fixed-bottom">
     <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="float-start">© <a href="https://thatseagullbob.co.uk" title="That Seagull Bob Homepage">That Seagull Bob <span id="current-year">2020</span></a></p>
+            </div>
+            <div class="col">
+                <p class="float-end">Company no: 11844082<br /><a href="https://ecologi.com/thatseagullbob" title="Ecologi Tree Planting">Visit our forest</a></p>
+            </div>
         </div>
     </div>
 </footer>
